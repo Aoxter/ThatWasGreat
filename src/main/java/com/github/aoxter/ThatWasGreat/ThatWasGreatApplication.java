6e93a3a50b9ba4fa22e105efeb0392aea1f2bbcb
Piotr Kopycki <pkopycki@onet.pl>
2023-05-01 12:49:26 +0200

    Init commit

---
+++ src/main/java/com/github/aoxter/ThatWasGreat/ThatWasGreatApplication.java
@@ -0,0 +1,13 @@
+package com.github.aoxter.ThatWasGreat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThatWasGreatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThatWasGreatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/aoxter/ThatWasGreat/ThatWasGreatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aoxter.ThatWasGreat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThatWasGreatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
