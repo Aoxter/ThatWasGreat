335f1e92d4a185bcbe042417c6a578e885527d2a
Piotr Kopycki <pkopycki@onet.pl>
2024-02-18 17:45:05 +0100

    Improve and clean GlobalExceptionHandler class

--- src/main/java/com/github/aoxter/ThatWasGreat/GlobalExceptionHandler.java
+++ src/main/java/com/github/aoxter/ThatWasGreat/GlobalExceptionHandler.java
@@ -11,11 +11,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.util.WebUtils;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
-
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = CategoryCanNotBeRemovedException.class)
     public final ResponseEntity<Object> handleCategoryCanNotBeRemovedException(CategoryCanNotBeRemovedException exception, WebRequest request) {
@@ -41,10 +40,9 @@ public class GlobalExceptionHandler {
     public final ResponseEntity<Object> handleFactorNotFoundException(FactorNotFoundException exception, WebRequest request) {
         return handleExceptionInternal(exception, exception.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
-    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, String body, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        if (HttpStatus.INTERNAL_SERVER_ERROR.equals(status)) {
-            request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, WebRequest.SCOPE_REQUEST);
-        }
-        return new ResponseEntity<>(body, headers, status);
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleUnexpectedException(Exception exception, WebRequest request) {
+        return handleExceptionInternal(exception, exception.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 }
